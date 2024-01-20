c7c5dd6bd882cd8757319610063f086399c28268
HyungWookChoi <qpflapffhs76@gmail.com>
2024-01-20 19:56:37 +0900

    Initial commit

---
+++ app/mock.ts
@@ -0,0 +1,27 @@
+export interface DocItem {
+  id: string;
+  name: string;
+}
+
+export const monsters: DocItem[] = [
+  {
+    id: '1130100',
+    name: 'Axe Stump',
+  },
+  {
+    id: '8140500',
+    name: 'Bain',
+  },
+  {
+    id: '4230116',
+    name: 'Barnard Gray',
+  },
+  {
+    id: '3210205',
+    name: 'Black Ratz',
+  },
+  {
+    id: '4230109',
+    name: 'Block Golem',
+  },
+];
