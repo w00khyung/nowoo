9da5bc40cd4bc41381964f9d663a5ab581d3afe8
HyungWookChoi <qpflapffhs76@gmail.com>
2024-01-22 21:08:27 +0900

    NULL 데이터가 포함되어 limit 추가함.

--- app/(route)/item/[slug]/page.tsx
+++ app/(route)/item/[slug]/page.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import supabase from '@/app/_lib/utils/supabase'
 
 export async function generateStaticParams() {
-  const { data: monsters } = await supabase.from('items').select()
+  const { data: items } = await supabase.from('items').select().limit(9)
 
-  return monsters?.map((monster) => ({
-    slug: monster.maple_item_id?.toString() ?? '',
+  if (!items) return []
+
+  return items?.map(({ maple_item_id }) => ({
+    slug: maple_item_id?.toString() ?? '',
   }))
 }
 
@@ -21,17 +23,19 @@ export default async function Page({
 
   const { data: item } = await supabase.from('items').select().eq('maple_item_id', slug).single()
 
+  if (!item) return null
+
   return (
     <main className='flex h-screen flex-col items-center justify-center gap-1'>
       <Image
         className='max-h-20 min-h-20  min-w-20 max-w-20 object-contain'
-        src={`https://maplestory.io/api/gms/62/item/${item?.maple_item_id}/icon?resize=3`}
+        src={`https://maplestory.io/api/gms/62/item/${item.maple_item_id}/icon?resize=3`}
         width={80}
         height={80}
-        alt={item?.name_kor ?? 'No Name'}
+        alt={item.name_kor ?? 'No Name'}
       />
-      <h1>{item?.name_kor}</h1>
-      <p>Item Id: {item?.maple_item_id}</p>
+      <h1>{item.name_kor}</h1>
+      <p>Item Id: {item.maple_item_id}</p>
     </main>
   )
 }
--- app/(route)/monster/[slug]/page.tsx
+++ app/(route)/monster/[slug]/page.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import supabase from '@/app/_lib/utils/supabase'
 
 export async function generateStaticParams() {
-  const { data: monsters } = await supabase.from('monsters').select()
+  const { data: monsters } = await supabase.from('monsters').select().limit(9)
 
-  return monsters?.map((monster) => ({
-    slug: monster.maple_mob_id?.toString() ?? '',
+  if (!monsters) return []
+
+  return monsters?.map(({ maple_mob_id }) => ({
+    slug: maple_mob_id?.toString() ?? '',
   }))
 }
 
@@ -21,17 +23,19 @@ export default async function Page({
 
   const { data: monster } = await supabase.from('monsters').select().eq('maple_mob_id', slug).single()
 
+  if (!monster) return null
+
   return (
     <main className='flex h-screen flex-col items-center justify-center gap-1'>
       <Image
         className='max-h-20 min-h-20  min-w-20 max-w-20 object-contain'
-        src={`https://maplestory.io/api/gms/62/mob/animated/${monster?.maple_mob_id}/move`}
+        src={`https://maplestory.io/api/gms/62/mob/animated/${monster.maple_mob_id}/move`}
         width={80}
         height={80}
-        alt={monster?.name_kor ?? 'No Name'}
+        alt={monster.name_kor ?? 'No Name'}
       />
-      <h1>{monster?.name_kor}</h1>
-      <p>Level: {monster?.level}</p>
+      <h1>{monster.name_kor}</h1>
+      <p>Level: {monster.level}</p>
     </main>
   )
 }
