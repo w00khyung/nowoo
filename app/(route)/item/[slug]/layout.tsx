cdcb91ada1c2639f4013f5bad79a0087dccbe928
w00khyung <qpflapffhs76@gmail.com>
2024-01-24 07:28:55 +0000

    메타데이터 타이틀 수정, 상세페이지에 generateMetadata 추가함. (#18)
    * 메타데이터 타이틀 수정함.
    * 아이템, 몬스터 상세페이지에 generateMetadata 추가함.

--- app/(route)/item/[slug]/layout.tsx
+++ app/(route)/item/[slug]/layout.tsx
@@ -1,4 +1,20 @@
 import Footer from '@/app/_components/shared/footer'
+import supabase from '@/app/_lib/utils/supabase'
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const { data: item } = await supabase
+    .from('items')
+    .select('name_kor, description_kor')
+    .match({
+      maple_item_id: params.slug,
+    })
+    .single()
+
+  return {
+    title: `${item?.name_kor || '아이템'} | NOWOO - 메이플랜드 아이템 검색 사이트`,
+    description: item?.description_kor || '메이플랜드 아이템 검색 사이트',
+  }
+}
 
 export default function RootLayout({
   children,
--- app/(route)/monster/[slug]/layout.tsx
+++ app/(route)/monster/[slug]/layout.tsx
@@ -1,4 +1,20 @@
 import Footer from '@/app/_components/shared/footer'
+import supabase from '@/app/_lib/utils/supabase'
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const { data: monster } = await supabase
+    .from('monsters')
+    .select('name_kor, description_kor')
+    .match({
+      maple_mob_id: params.slug,
+    })
+    .single()
+
+  return {
+    title: `${monster?.name_kor || '몬스터'} | NOWOO - 메이플랜드 아이템 검색 사이트`,
+    description: monster?.description_kor || '메이플랜드 아이템 검색 사이트',
+  }
+}
 
 export default function RootLayout({
   children,
