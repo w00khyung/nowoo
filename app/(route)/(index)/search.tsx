c11a9c2932b14d37b95de53dc8422e7da6673385
w00khyung <qpflapffhs76@gmail.com>
2024-01-24 13:27:48 +0000

    검색창 UI 수정함. (#24)

--- app/(route)/(index)/search.tsx
+++ app/(route)/(index)/search.tsx
@@ -6,7 +6,6 @@ import { Tables } from '@/app/_types/supabase'
 
 import SearchBar from './search-bar'
 import SearchResult from './search-result'
-import SearchResultLoader from './search-result-loader'
 
 interface Props {
   items: Tables<'items'>[]
@@ -20,24 +19,18 @@ export default function Search({ items, monsters }: Readonly<Props>) {
     setSearchValue(value)
   }
 
+  const searchedItems = items.filter((item) => item.name_kor?.includes(searchValue)).slice(0, 5)
+  const searchedMonsters = monsters.filter((monster) => monster.name_kor?.includes(searchValue)).slice(0, 5)
+
   return (
     <div className='relative flex w-[600px] justify-center max-sm:w-full'>
       <SearchBar
-        isItemExist={
-          Boolean(searchValue) &&
-          (items.some((item) => item.name_kor?.includes(searchValue)) ||
-            monsters.some((monster) => monster.name_kor?.includes(searchValue)))
-        }
+        isItemExist={Boolean(searchValue && (searchedItems.length > 0 || searchedMonsters.length > 0))}
         searchValue={searchValue}
         onChangeSearchValue={onChangeSearchValue}
       />
-      <Suspense fallback={<SearchResultLoader />}>
-        <SearchResult
-          items={searchValue ? items.filter((item) => item.name_kor?.includes(searchValue)).slice(0, 5) : []}
-          monsters={
-            searchValue ? monsters.filter((monster) => monster.name_kor?.includes(searchValue)).slice(0, 5) : []
-          }
-        />
+      <Suspense>
+        <SearchResult items={searchValue ? searchedItems : []} monsters={searchValue ? searchedMonsters : []} />
       </Suspense>
     </div>
   )
