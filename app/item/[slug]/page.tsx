eecd0c7a512d6a0dab75a347545ac37c1dfd6886
w00khyung <qpflapffhs76@gmail.com>
2024-01-28 15:53:45 +0000

    style : ItemPage Design Refactoring

--- app/item/[slug]/page.tsx
+++ app/item/[slug]/page.tsx
@@ -35,32 +35,32 @@ export default async function Page({ params }: Readonly<Props>) {
       <Logo />
       <Menu />
       <SearchForm />
-      <div className='mt-24 flex w-[500px] max-w-full flex-col bg-[#06062C] bg-opacity-50 px-2 pb-20 text-white shadow-md'>
+      <div className='mt-24 flex w-[500px] max-w-full flex-col bg-[#06062C] bg-opacity-50 px-2 pb-5 text-white shadow-md'>
         <div className='mt-5 flex w-full flex-col items-center gap-1 p-2'>
           <h1 className='text-3xl font-semibold'>{item.name_kor}</h1>
         </div>
 
-        <div className='flex gap-10 px-4 py-4'>
+        <div className='flex gap-12 px-4 py-4'>
           <div className='h-fit bg-white bg-opacity-30 p-2'>
             <Image
               className='aspect-square object-contain'
               src={getItemImage(item.maple_item_id)}
-              width={100}
-              height={100}
+              width={160}
+              height={160}
               alt={item.name_kor}
             />
           </div>
           <div className='flex flex-col'>
-            <span className='text-sm'>REQ LEV: {item.req_level}</span>
-            <span className='text-sm'>REQ STR: {item.req_str}</span>
-            <span className='text-sm'>REQ DEX: {item.req_dex}</span>
-            <span className='text-sm'>REQ INT: {item.req_int}</span>
-            <span className='text-sm'>REQ LUK: {item.req_luk}</span>
-            <span className='text-sm'>REQ POP: {item.req_pop}</span>
+            <span className='mb-2 text-sm'>REQ LEV : {item.req_level}</span>
+            <span className='mb-2 text-sm'>REQ STR : {item.req_str}</span>
+            <span className='mb-2 text-sm'>REQ DEX : {item.req_dex}</span>
+            <span className='mb-2 text-sm'>REQ INT : {item.req_int}</span>
+            <span className='mb-2 text-sm'>REQ LUK : {item.req_luk}</span>
+            <span className='mb-2 text-sm'>REQ POP : {item.req_pop}</span>
           </div>
         </div>
 
-        <div className='mb-2 flex justify-center gap-10 border-b-2 border-b-white pb-2 max-md:gap-4'>
+        <div className='mb-5 flex justify-center gap-14 border-b-2 border-b-white pb-2 max-md:gap-4'>
           {['초보자', '전사', '마법사', '궁수', '도적'].map((job) => (
             <span key={job} className={cn('text-lg', JOB[item.req_job].includes(job) ? '' : 'text-red-500')}>
               {job}
@@ -80,11 +80,9 @@ export default async function Page({ params }: Readonly<Props>) {
             {Boolean(item.inc_luk) && <span>LUK: +{item.inc_luk}</span>}
             {Boolean(item.inc_hp) && <span>HP: +{item.inc_hp}</span>}
             {Boolean(item.inc_mp) && <span>MP: +{item.inc_mp}</span>}
-            <span>업그레이드 가능 횟수: {item.upgradable_count}</span>
-          </div>
-          <div className='flex flex-col gap-1'>
-            <span>상점 거래가: {item.price_shop} 메소</span>
-            {/* <span>거래 시세가: {item.price_shop} 메소</span> */}
+            <span>업그레이드 가능 횟수 : {item.upgradable_count}</span>
+            <span>상점 거래가 : {item.price_shop} 메소</span>
+            <span>거래 시세가 : {item.price_average ? `${item.price_average} 메소` : `데이터 준비중입니다.`}</span>
           </div>
         </div>
       </div>
