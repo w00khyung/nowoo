50eb2ef168045121e2ef0c6d59b405c6985d4ed0
w00khyung <qpflapffhs76@gmail.com>
2024-01-31 02:12:23 +0000

    랜덤 4자리 난수 생성 로직 수정함.

--- app/board/free/create/action.ts
+++ app/board/free/create/action.ts
@@ -4,12 +4,19 @@ import * as argon2 from 'argon2'
 
 import supabase from '@/lib/utils/supabase'
 
+function getRandom4DigitNumber() {
+  const array = new Uint16Array(1)
+  crypto.getRandomValues(array)
+  const randomNumber = array[0] % 10000
+  return String(randomNumber).padStart(4, '0')
+}
+
 export async function createBoard({ title, content, password }: { title: string; content: string; password: string }) {
   const response = await supabase.from('boards').insert([
     {
       title,
       description: content,
-      writer: `nowoo${Math.floor(1000 + Math.random() * 9000)}`,
+      writer: `nowoo${getRandom4DigitNumber()}`,
       password: await argon2.hash(password),
     },
   ])
