86a4efd006a88aa835264781fa08ce8e3bcc7a01
w00khyung <qpflapffhs76@gmail.com>
2024-02-05 12:12:48 +0000

    인기 몬스터 및 아이템 스켈레톤 UI 적용함.

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -5,6 +5,7 @@ import supabase from '@/lib/utils/supabase'
 
 import PopularItems from './popular-items'
 import PopularMonsters from './popular-monsters'
+import { PopularSkeletonUI } from './popular-skeleton-ui'
 
 export default async function HomePage() {
   const header = headers()
@@ -35,8 +36,10 @@ export default async function HomePage() {
 
   return (
     <div className='mt-24 flex w-full gap-10 max-lg:flex-col'>
-      <Suspense>
+      <Suspense fallback={<PopularSkeletonUI title='다른 유저들이 선호하는 아이템' />}>
         <PopularItems />
+      </Suspense>
+      <Suspense fallback={<PopularSkeletonUI title='인기 몬스터' />}>
         <PopularMonsters />
       </Suspense>
     </div>
